2fff1b0b1c6353bb4a46909c3d85ecab41f22a20
Gabriel <gabrieldiegues12@gmail.com>
2025-10-21 23:16:14 -0300

    Delete user feature added

--- src/Components/Logout.tsx
+++ src/Components/Logout.tsx
@@ -2,12 +2,17 @@ import { DrawerContentComponentProps, DrawerContentScrollView, DrawerItem, Drawe
 import { useNavigation } from '@react-navigation/native';
 import { AppStackParamList } from '../Types/Navigation';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { WealthPilotApi } from '../api';
+import { useEventContext } from '../Navigation/Context/EventContext';
+import { getAuth } from '@react-native-firebase/auth';
 
 
 const Logout = (props: DrawerContentComponentProps) => {
+    // Inner variables
+    const {firebaseUid} = useEventContext();
     const navigation = useNavigation<NativeStackNavigationProp<AppStackParamList>>();
 
-
+    // Inner functions
     const handleLogout = () => {
         return navigation.reset({
             index: 0,
@@ -16,9 +21,23 @@ const Logout = (props: DrawerContentComponentProps) => {
     };
 
 
+    const deleteUser = async () => {
+        const currentUser = getAuth().currentUser;
+        if(currentUser) {
+            await currentUser.delete().
+            then(async () => {
+                await WealthPilotApi.delete(`/users/delete/${firebaseUid}`, {
+                    timeout: 500000,
+                });
+            }).finally(() => handleLogout());
+        }
+    };
+
+
     return(
         <DrawerContentScrollView>
             <DrawerItemList {...props}/>
+            <DrawerItem label="deletar conta" onPress={deleteUser}/>
             <DrawerItem label="logout" onPress={handleLogout} />
         </DrawerContentScrollView>
     );
